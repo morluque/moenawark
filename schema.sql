eb3094a5c2e98a167c1256d598fe1004b4fb1006
morluque <lerougearlo@gmail.com>
2017-10-26 22:15:39 +0200

    SQL syntax for FOREIGN KEYs
    Damn, can't remember that syntax...

--- schema.sql
+++ schema.sql
@@ -14,11 +14,11 @@ CREATE TABLE users (
 	password TEXT NOT NULL,
 	registered BOOLEAN NOT NULL DEFAULT false,
 	game_master BOOLEAN NOT NULL DEFAULT false,
-	character_id INTEGER DEFAAULT NULL FOREIGN KEY REFERENCES characters(id)
+	character_id INTEGER DEFAULT NULL CONSTRAINT fk_user_char REFERENCES characters(id)
 );
 
 CREATE TABLE registrations (
-	user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+	user_id INTEGER NOT NULL CONSTRAINT fk_reg_user REFERENCES users(id),
 	valid_until INTEGER NOT NULL,
 	token TEXT NOT NULL,
 	status INTEGER NOT NULL DEFAULT 0, -- generated=0, sent=1, error=2
@@ -33,15 +33,15 @@ CREATE TABLE places (
 );
 
 CREATE TABLE wormholes (
-	source_id INTEGER NOT NULL FOREIGN KEY REFERENCES places(id)
-	destination_id INTEGER NOT NULL FOREIGN KEY REFERENCES places(id)
+	source_id INTEGER NOT NULL CONSTRAINT fk_wormh_source REFERENCES places(id),
+	destination_id INTEGER NOT NULL CONSTRAINT fk_wormh_dest REFERENCES places(id),
 	distance INTEGER NOT NULL,
 	CONSTRAINT pk_wormholes PRIMARY KEY (source_id, destination_id)
 );
 
 CREATE TABLE objects (
 	id INTEGER PRIMARY KEY NOT NULL,
-	place_id INTEGER NOT NULL FOREIGN KEY REFERENCES places(id)
+	place_id INTEGER NOT NULL CONSTRAINT fk_obj_place REFERENCES places(id)
 );
 
 CREATE TABLE atoms (
@@ -52,24 +52,24 @@ CREATE TABLE atoms (
 CREATE TABLE matters (
 	id INTEGER PRIMARY KEY NOT NULL,
 	quantity INTEGER NOT NULL CONSTRAINT notvoid CHECK (quantity > 0),
-	atom_id INTEGER FOREIGN KEY REFERENCES atoms(id),
+	atom_id INTEGER NOT NULL CONSTRAINT fk_matter_atom REFERENCES atoms(id)
 );
 
 CREATE TABLE resources (
-	object_id INTEGER UNIQUE NOT NULL FOREIGN KEY REFERENCES objects(id),
-	volume INTEGER NOT NULL CONSTRAINT volume_positive CHECK (quantity > 0),
-	sturdiness INTEGER NOT NULL CONSTRAINT sturdiness_positive CHECK (quantity > 0),
+	object_id INTEGER UNIQUE NOT NULL CONSTRAINT fk_res_obj REFERENCES objects(id),
+	volume INTEGER NOT NULL CONSTRAINT volume_positive CHECK (volume > 0),
+	sturdiness INTEGER NOT NULL CONSTRAINT sturdiness_positive CHECK (sturdiness > 0),
 	CONSTRAINT pk_resources PRIMARY KEY (object_id)
 );
 
 CREATE TABLE resource_components (
-	resource_id INTEGER NOT NULL FOREIGN KEY REFERENCES resources(id),
-	matter_id INTEGER NOT NULL FOREIGN KEY REFERENCES matters(id),
+	resource_id INTEGER NOT NULL CONSTRAINT fk_res_comp_res REFERENCES resources(id),
+	matter_id INTEGER NOT NULL CONSTRAINT fk_res_comp_matter REFERENCES matters(id),
 	CONSTRAINT pk_res_comp PRIMARY KEY (resource_id, matter_id)
 );
 
 CREATE TABLE constructions (
-	resource_id INTEGER UNIQUE NOT NULL FOREIGN KEY REFERENCES resources(id),
+	resource_id INTEGER UNIQUE NOT NULL CONSTRAINT fk_constr_res REFERENCES resources(id),
 	group_count INTEGER NOT NULL DEFAULT 1,
 	name TEXT NOT NULL,
 	attack INTEGER NOT NULL DEFAULT 0,
@@ -82,20 +82,20 @@ CREATE TABLE constructions (
 );
 
 CREATE TABLE construction_freight (
-	construction_id INTEGER NOT NULL FOREIGN KEY REFERENCES constructions(id),
-	object_id INTEGER NOT NULL FOREIGN KEY REFERENCES objects(id),
+	construction_id INTEGER NOT NULL CONSTRAINT fk_constr_freight_constr REFERENCES constructions(id),
+	object_id INTEGER NOT NULL CONSTRAINT fk_constr_freight_obj REFERENCES objects(id),
 	CONSTRAINT pk_constr_freight PRIMARY KEY (construction_id, object_id)
 );
 
 CREATE TABLE construction_biocompatibility (
-	construction_id INTEGER NOT NULL FOREIGN KEY REFERENCES constructions(id),
-	matter_id INTEGER NOT NULL FOREIGN KEY REFERENCES matters(id),
+	construction_id INTEGER NOT NULL CONSTRAINT fk_constr_bioc_constr REFERENCES constructions(id),
+	matter_id INTEGER NOT NULL CONSTRAINT fk_constr_bioc_matter REFERENCES matters(id),
 	CONSTRAINT pk_constr_bioc PRIMARY KEY (construction_id, matter_id)
 );
 
 CREATE TABLE entities (
-	resource_id INTEGER NOT NULL FOREIGN KEY REFERENCES resources(id),
-	character_id INTEGER DEFAULT NULL FOREIGN KEY REFERENCES characters(id),
+	resource_id INTEGER NOT NULL CONSTRAINT fk_ent_res REFERENCES resources(id),
+	character_id INTEGER DEFAULT NULL CONSTRAINT fk_ent_char REFERENCES characters(id),
 	group_count INTEGER NOT NULL DEFAULT 1,
 	name TEXT NOT NULL,
 	trust INTEGER NOT NULL DEFAULT 0,
@@ -111,15 +111,15 @@ CREATE TABLE knowledge_domains (
 );
 
 CREATE TABLE knowledge_constraints (
-	target_domain_id INTEGER NOT NULL FOREIGN KEY REFERENCES knowledge_domains(id),
-	prerequisite_domain_id INTEGER NOT NULL FOREIGN KEY REFERENCES knowledge_domains(id),
+	target_domain_id INTEGER NOT NULL CONSTRAINT fk_knwl_constr_target REFERENCES knowledge_domains(id),
+	prerequisite_domain_id INTEGER NOT NULL CONSTRAINT fk_knwl_constr_prereq REFERENCES knowledge_domains(id),
 	minimum_proficiency INTEGER NOT NULL,
 	CONSTRAINT pk_kn_cstr PRIMARY KEY (target_domain_id, prerequisite_domain_id)
 );
 
 CREATE TABLE knowledges (
-	entity_id INTEGER NOT NULL FOREIGN KEY REFERENCES entities(id),
-	knowledge_domain_id INTEGER NOT NULL FOREIGN KEY REFERENCES knowledge_domains(id),
+	entity_id INTEGER NOT NULL CONSTRAINT fk_knwl_ent REFERENCES entities(id),
+	knowledge_domain_id INTEGER NOT NULL CONSTRAINT fk_knwl_dom REFERENCES knowledge_domains(id),
 	proficiency INTEGER NOT NULL
 );
 
@@ -132,9 +132,10 @@ CREATE TABLE turns (
 
 CREATE TABLE orders (
 	id INTEGER PRIMARY KEY NOT NULL,
-	turn_id INTEGER NOT NULL FOREIGN KEY REFERENCES turns(id),
-	character_id INTEGER NOT NULL FOREIGN KEY REFERENCES characters(id),
+	turn_id INTEGER NOT NULL CONSTRAINT fk_order_turn REFERENCES turns(id),
+	character_id INTEGER NOT NULL CONSTRAINT fk_order_char REFERENCES characters(id),
 	cost INTEGER NOT NULL,
 	order_type TEXT NOT NULL,
 	json_args TEXT NOT NULL
 );
+
