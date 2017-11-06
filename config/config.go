dfa7ad4df1f32055f0d597073e3ad7273d049786
morluque <lerougearlo@gmail.com>
2017-11-06 19:19:05 +0100

    Use a TOML config file.

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/BurntSushi/toml"
+	"os"
+)
+
+type Config struct {
+	DBPath string `toml:"db_path"`
+}
+
+func Parse(path string) (*Config, error) {
+	var conf Config
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+	if _, err := toml.DecodeFile(path, &conf); err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/morluque/moenawark/config"
 	"github.com/morluque/moenawark/model/character"
 	"github.com/morluque/moenawark/model/user"
 	"github.com/morluque/moenawark/sqlstore"
@@ -11,9 +12,14 @@ import (
 )
 
 func main() {
-	var dbPath = flag.String("dbpath", "data/db/moenawark.sqlite", "path to DB file")
+	var configPath = flag.String("cfg", "moenawark.toml", "path to TOML config file")
 	flag.Parse()
-	fmt.Printf("DB path: %s\n", dbPath)
+	fmt.Printf("config path: %s\n", *configPath)
+
+	conf, err := config.Parse(*configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	c := character.New("Foo", 10, 5)
 	u := user.New("foo@example.com", "secret")
@@ -23,7 +29,13 @@ func main() {
 		fmt.Printf("c: %v\n", u.Character)
 	}
 
-	db, err := sqlstore.Open(*dbPath)
+	var dbPath string
+	if len(conf.DBPath) > 0 {
+		dbPath = conf.DBPath
+	} else {
+		dbPath = "data/db/moenawark.sqlite"
+	}
+	db, err := sqlstore.Open(dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
