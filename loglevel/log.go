d615de025829682b342ba830bdcd0e75d3580ba0
morluque <lerougearlo@gmail.com>
2017-12-04 11:41:42 +0100

    golint loglevel + implement Fatal()

--- loglevel/log.go
+++ loglevel/log.go
@@ -7,27 +7,33 @@ import (
 	"os"
 )
 
+// Level represents of log level as an unsigned integer
 type Level uint8
 
-type LevelFilter struct {
-	Levels   map[Level]string
-	MinLevel Level
-	Writer   io.Writer
+type levelFilter struct {
+	levels   map[Level]string
+	minLevel Level
+	writer   io.Writer
 }
 
+// Logger is a logger with a minimum level; only messages above that level will be printed.
 type Logger struct {
 	logger *log.Logger
-	filter LevelFilter
+	filter levelFilter
 	prefix string
 }
 
+// Available semi-standard log levels
 const (
-	Debug = iota
+	Unknown = iota
+	Debug
 	Info
 	Warn
 	Error
+	None
 )
 
+// New returns a new Logger with given prefix and minimum level.
 func New(prefix string, minLevel Level) *Logger {
 	l := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.LUTC)
 	levels := make(map[Level]string)
@@ -35,41 +41,52 @@ func New(prefix string, minLevel Level) *Logger {
 	levels[Info] = "info"
 	levels[Warn] = "warn"
 	levels[Error] = "error"
-	filter := LevelFilter{
-		Levels:   levels,
-		MinLevel: minLevel,
-		Writer:   os.Stdout,
+	filter := levelFilter{
+		levels:   levels,
+		minLevel: minLevel,
+		writer:   os.Stdout,
 	}
 
 	return &Logger{logger: l, filter: filter, prefix: prefix}
 }
 
+// SetLevel dynamically sets the minimul log level
 func (l *Logger) SetLevel(level Level) {
-	l.filter.MinLevel = level
+	l.filter.minLevel = level
 }
 
 func (l *Logger) printf(level Level, format string, v ...interface{}) {
-	if l.filter.MinLevel >= level {
-		l.logger.Printf(fmt.Sprintf("[%s] %s: %s", l.filter.Levels[level], l.prefix, format), v...)
+	if l.filter.minLevel >= level {
+		l.logger.Printf(fmt.Sprintf("[%s] %s: %s", l.filter.levels[level], l.prefix, format), v...)
 	}
 }
 
+// Debugf prints a formatted message if minimum level is less than or equal to Debug.
 func (l *Logger) Debugf(format string, v ...interface{}) {
 	l.printf(Debug, format, v...)
 }
 
+// Infof prints a formatted message if minimum level is less than or equal to Info.
 func (l *Logger) Infof(format string, v ...interface{}) {
 	l.printf(Info, format, v...)
 }
 
+// Warnf prints a formatted message if minimum level is less than or equal to Warn.
 func (l *Logger) Warnf(format string, v ...interface{}) {
 	l.printf(Warn, format, v...)
 }
 
+// Errorf prints a formatted message if minimum level is less than or equal to Error.
 func (l *Logger) Errorf(format string, v ...interface{}) {
 	l.printf(Error, format, v...)
 }
 
+// Fatalf calls the underlying log.Logger's method Fatalf(); this will terminate your program.
 func (l *Logger) Fatalf(format string, v ...interface{}) {
 	l.logger.Fatalf(format, v...)
+}
+
+// Fatal calls the underlying log.Logger's method Fatal(); this will terminate your program.
+func (l *Logger) Fatal(v ...interface{}) {
+	l.logger.Fatal(v...)
 }
